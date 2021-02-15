39282703b11d0eff5589cd6b680c2685fb4c842e
Alicja Kaszuba <kaszubaalicja.96@gmail.com>
2021-02-15 12:01:15 +0100

    Prepare JS script for further work

--- js/script.js
+++ js/script.js
@@ -3,48 +3,54 @@
     const links = document.querySelectorAll('.titles a');
     console.log('links:', links);
   });*/
+{
+    // Wyświetlanie artykułów po kliknięciu
+    const titleClickHandler = function(event){
+        /* zawsze to dadać przy funkcjach - handlerach - po kliknięciu */
+        const clickedElement = this;
 
-const titleClickHandler = function(event){
-    /* zawsze to dadać przy funkcjach - handlerach - po kliknięciu */
-    const clickedElement = this;
+        console.log('Link was clicked!');
+        console.log(event);
 
-    console.log('Link was clicked!');
-    console.log(event);
+        event.preventDefault();
 
-    /* [DONE] remove class 'active' from all article links  */
-    const activeLinks = document.querySelectorAll('.titles a.active');
+        /* [DONE] remove class 'active' from all article links  */
+        const activeLinks = document.querySelectorAll('.titles a.active');
 
-    for(let activeLink of activeLinks){
-        activeLink.classList.remove('active');
+        for(let activeLink of activeLinks){
+            activeLink.classList.remove('active');
+        }
+
+        /* [DONE] add class 'active' to the clicked link */
+        console.log('clickedElement:', clickedElement); // console.log('clickedElement (with plus): ' + clickedElement);
+        clickedElement.classList.add('active');
+        
+        /* [DONE] remove class 'active' from all articles */
+        const activeArticles = document.querySelectorAll('.posts article.active');
+
+        for(let activeArticle of activeArticles){
+            activeArticle.classList.remove('active');
+        }
+
+        /* [DONE] get 'href' attribute from the clicked link */
+        const articleName = clickedElement.getAttribute('href');
+        console.log('Href attribute: ', articleName);
+
+        /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+        const clickedArticle = document.querySelector('.posts ' + articleName);
+        console.log('clickedArticle: ', clickedArticle);
+
+        /* [DONE] add class 'active' to the correct article */
+        clickedArticle.classList.add('active');
     }
 
-    /* [IN PROGRESS] add class 'active' to the clicked link */
-    console.log('clickedElement:', clickedElement);
-    // console.log('clickedElement (with plus): ' + clickedElement);
-    clickedElement.classList.add('active');
-    
-    /* [DONE] remove class 'active' from all articles */
-    const activeArticles = document.querySelectorAll('.posts article.active');
+    const links = document.querySelectorAll('.titles a');
 
-    for(let activeArticle of activeArticles){
-        activeArticle.classList.remove('active');
+    for(let link of links){
+        link.addEventListener('click', titleClickHandler);
     }
-
-    /* get 'href' attribute from the clicked link */
-    const articleName = clickedElement.getAttribute('href');
-    console.log('Href attribute: ', articleName);
-
-    /* find the correct article using the selector (value of 'href' attribute) */
-    const clickedArticle = document.querySelector('.posts ' + articleName);
-    console.log('clickedArticle: ', clickedArticle);
-
-    /* add class 'active' to the correct article */
-    clickedArticle.classList.add('active');
 }
+{
+    // Generowanie listy tytułów
 
-const links = document.querySelectorAll('.titles a');
-
-for(let link of links){
-    link.addEventListener('click', titleClickHandler);
 }
-
